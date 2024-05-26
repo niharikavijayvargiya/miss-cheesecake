cac3286d690881030d4ee0da5d5605e5078b2b16
Apple <apple@Apples-MacBook-Pro.local>
2024-05-27 00:45:52 +0530

    corrected phone number and mail

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,12 @@ function Footer() {
   return (
     <footer>
       <div className="social-media">
-        <a href="https://www.instagram.com"><FaInstagram /></a>
+        <a href="https://www.instagram.com/slice_of_joy_11/?igsh=aTRtY2V3aW9vdHpo"><FaInstagram /></a>
         <a href="https://www.whatsapp.com"><FaWhatsapp /></a>
         <a href="mailto:info@misscheesecake.com"><FaEnvelope /></a>
-        <a href="tel:+1234567890"><FaPhone /></a>
+        <a href="tel:+91-9972967150"><FaPhone /></a>
       </div>
-      <p>Contact us at: info@misscheesecake.com | +1234567890</p>
+      <p>Contact us at: info@misscheesecake.com | +91-9972967150</p>
     </footer>
   );
 }
